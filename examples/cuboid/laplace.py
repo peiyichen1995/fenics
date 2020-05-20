526e12b35ae85390d8dc916ebbcec41e2fdc6d35
Peiyi Chen <peiyic03@gmail.com>
2020-05-20 10:05:14 -0400

    laplcae equation for box

--- examples/cuboid/laplace.py
+++ examples/cuboid/laplace.py
@@ -19,10 +19,7 @@ output_dir = "./output/"
 mesh_dir = "./mesh/"
 
 # mesh
-cylinder_o = Cylinder(Point(0, 0, 0), Point(0, 0, 3), 2, 2)
-cylinder_i = Cylinder(Point(0, 0, 0), Point(0, 0, 3), 1, 1)
-geometry = cylinder_o - cylinder_i
-mesh = generate_mesh(geometry, 10)
+mesh = BoxMesh(Point(0.0, 0.0, 0.0), Point(10., 5., 1.), 10, 10, 10)
 
 # function space
 V = FunctionSpace(mesh, 'CG', 2)
@@ -34,11 +31,9 @@ phi = Function(V)
 
 # mark boundary subdomians
 bottom = CompiledSubDomain("near(x[2], side) && on_boundary", side=0.0)
-top = CompiledSubDomain("near(x[2], side) && on_boundary", side=3.0)
-inner = CompiledSubDomain(
-    "near(x[0]*x[0]+x[1]*x[1], side, 0.1) && on_boundary", side=1.0)
-outer = CompiledSubDomain(
-    "near(x[0]*x[0]+x[1]*x[1], side, 0.1) && on_boundary", side=4.0)
+top = CompiledSubDomain("near(x[2], side) && on_boundary", side=1.0)
+left = CompiledSubDomain("near(x[0], side) && on_boundary", side=0.0)
+right = CompiledSubDomain("near(x[0], side) && on_boundary", side=10.0)
 
 # write mesh
 mesh_file = File(mesh_dir + "mesh.xml")
@@ -47,10 +42,10 @@ mesh_file << mesh
 # boundary conditions
 bc_top = DirichletBC(V, Constant(1.0), top)
 bc_bottom = DirichletBC(V, Constant(0.0), bottom)
-bc_inner = DirichletBC(V, Constant(0.0), inner)
-bc_outer = DirichletBC(V, Constant(1.0), outer)
+bc_left = DirichletBC(V, Constant(0.0), left)
+bc_right = DirichletBC(V, Constant(1.0), right)
 bcs_1 = [bc_top, bc_bottom]
-bcs_2 = [bc_inner, bc_outer]
+bcs_2 = [bc_left, bc_right]
 
 # variational problem
 Pi = 0.5 * dot(grad(phi), grad(phi)) * dx
