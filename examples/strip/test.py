7f7d5573ab58586fc05fea2788693c21c3b93ab7
Peiyi Chen <peiyic03@gmail.com>
2020-06-17 19:13:30 -0400

    incompressible uniaxial test

--- examples/strip/test.py
+++ examples/strip/test.py
@@ -6,31 +6,69 @@ import dolfin as dlf
 import numpy as np
 import math
 
+from utils import my_cross, matrix_cofactor
+
+
 parameters["form_compiler"]["cpp_optimize"] = True
 parameters["form_compiler"]["representation"] = "uflacs"
+
+
+# traction
+class Traction(UserExpression):
+    def __init__(self):
+        super().__init__(self)
+        self.t = 0.0
+
+    def eval(self, values, x):
+        values[0] = 0 * self.t
+        values[1] = 0.0
+        values[2] = 0.0
+
+    def value_shape(self):
+        return (3,)
 
 # Kinematics
 
 
-def pk1Stress(u, pressure, E, nu):
+def NeoHookean_imcompressible(mu1, mu2, mu3, mu4, beta3, beta4, a1, a2, F):
+    J = det(F)
+    C = F.T * F
+    temp1 = mu1 * tr(C) / pow(J, 2 / 3)
+    temp2 = mu2 * pow(sqrt(tr(matrix_cofactor(F).T *
+                              matrix_cofactor(F))), 3) / pow(J, 2)
+    h = pow(J, beta3) + pow(J, -beta3)
+    temp3 = mu3 * h
+    temp4 = mu4 / beta4 * \
+        exp(beta4 * pow(conditional(gt(inner(F * a1, F * a1), 1),
+                                    inner(F * a1, F * a1) - 1, 0), 2))
+    temp5 = mu4 / beta4 * \
+        exp(beta4 * pow(conditional(gt(inner(F * a2, F * a2), 1),
+                                    inner(F * a2, F * a2) - 1, 0), 2))
+    return temp1 + temp2 + temp3 + temp4 + temp5
+
+
+def pk1Stress(u, pressure, E, nu, mu1, mu2, mu3, mu4, beta3, beta4, a1, a2):
     G = E / (2 * (1 + nu))
     c1 = G / 2.0
 
     I = Identity(V.mesh().geometry().dim())  # Identity tensor
     F = I + grad(u)          # Deformation gradient
-    C = F.T * F                # Right Cauchy-Green tensor
+    # C = F.T * F                # Right Cauchy-Green tensor
+    C = variable(F.T * F)
     Ic = tr(C)               # Invariants of deformation tensors
     J = det(F)
     pk2 = 2 * c1 * I - pressure * inv(C)  # second PK stress
+    NH = NeoHookean_imcompressible(mu1, mu2, mu3, mu4, beta3, beta4, a1, a2, F)
+    PK2 = 2.0 * diff(NH, C)
     return pk2, (J - 1)
 
 
-def geometry_3d():
-    mesh = UnitCubeMesh(6, 6, 6)
+def geometry_3d(mesh_dir):
+    mesh = Mesh(mesh_dir)
 
     boundary_parts = MeshFunction('size_t', mesh, mesh.topology().dim() - 1)
     x0 = AutoSubDomain(lambda x: near(x[0], 0))
-    x1 = AutoSubDomain(lambda x: near(x[0], 1))
+    x1 = AutoSubDomain(lambda x: near(x[0], 10))
     y0 = AutoSubDomain(lambda x: near(x[1], 0))
     z0 = AutoSubDomain(lambda x: near(x[2], 0))
     x0.mark(boundary_parts, 1)
@@ -41,7 +79,10 @@ def geometry_3d():
 
 
 # Create mesh and define function space ============================================
-facet_function = geometry_3d()
+output_dir = "./output/"
+mesh_dir = "./mesh/"
+
+facet_function = geometry_3d(mesh_dir + "mesh.xml")
 mesh = facet_function.mesh()
 gdim = mesh.geometry().dim()
 dx = Measure("dx")
@@ -49,15 +90,49 @@ ds = Measure("ds", subdomain_data=facet_function, subdomain_id=4)
 print('Number of nodes: ', mesh.num_vertices())
 print('Number of cells: ', mesh.num_cells())
 
+# ===============================================
+
+V = FunctionSpace(mesh, 'CG', 2)
+phi1 = Function(V)
+phi2 = Function(V)
+
+# read in laplace solutions
+phi1_h5 = HDF5File(MPI.comm_world, output_dir + "phi1.h5", "r")
+phi2_h5 = HDF5File(MPI.comm_world, output_dir + "phi2.h5", "r")
+phi1_h5.read(phi1, "phi1")
+phi2_h5.read(phi2, "phi2")
+phi1_h5.close()
+phi2_h5.close()
+
+# define orthorgonal basis
+e3 = grad(phi1)
+e1 = grad(phi2)
+e2 = my_cross(e3, e1)
+
+# normalize basis
+e1 = e1 / sqrt(inner(e1, e1))
+e2 = e2 / sqrt(inner(e2, e2))
+e3 = e3 / sqrt(inner(e3, e3))
+
+# define tissue orientation on the spatial varying basis
+theta = math.pi / 6
+a1 = math.cos(theta) * e3 + math.sin(theta) * e2
+a2 = math.cos(theta) * e3 - math.sin(theta) * e2
+
+
+# ===============================================
+
 # Limit quadrature degree
 dx = dx(degree=4)
 ds = ds(degree=4)
+
 
 # Create function space
 element_v = VectorElement("P", mesh.ufl_cell(), 1)
 element_s = FiniteElement("P", mesh.ufl_cell(), 1)
 mixed_element = MixedElement([element_v, element_s])
 V = FunctionSpace(mesh, mixed_element)
+
 
 # Define test and trial functions
 dup = TrialFunction(V)
@@ -66,13 +141,17 @@ _u, _p = TestFunctions(V)
 _u_p = Function(V)
 u, p = split(_u_p)
 
+
 # Create tensor function spaces for stress and stretch output
 W_DFnStress = TensorFunctionSpace(mesh, "DG", degree=0)
 defGrad = Function(W_DFnStress, name='F')
 PK1_stress = Function(W_DFnStress, name='PK1')
 
+
 # Displacement from previous iteration
 b = Constant((0.0, 0.0, 0.0))  # Body force per unit mass
+h = Traction()  # Traction force on the boundary
+
 
 # Define Dirichlet boundary
 bc0 = DirichletBC(V.sub(0).sub(0), Constant(0.), facet_function, 1)
@@ -85,13 +164,20 @@ bcs = [bc0, bc1, bc2, bc3]
 
 # material parameters
 E, nu = 1e3, 0.5
+mu1 = 4.1543
+mu2 = 2.5084
+mu3 = 9.7227
+mu4 = 19.285
+beta3 = 3.6537
+beta4 = 500.02
+g3 = 9.7227
 
-# Total potential energy
-pkstrs, hydpress = pk1Stress(u, p, E, nu)
+# # Total potential energy
+pkstrs, hydpress = pk1Stress(
+    u, p, E, nu, mu1, mu2, mu3, mu4, beta3, beta4, a1, a2)
 I = Identity(V.mesh().geometry().dim())
 dgF = I + grad(u)
-F1 = inner(dot(dgF, pkstrs), grad(_u)) * dx - \
-    dot(b, _u) * dx
+F1 = inner(dot(dgF, pkstrs), grad(_u)) * dx - dot(b, _u) * dx - dot(h, _u) * ds
 F2 = hydpress * _p * dx
 F = F1 + F2
 J = derivative(F, _u_p, dup)
@@ -101,6 +187,7 @@ problem = NonlinearVariationalProblem(F, _u_p, bcs=bcs, J=J)
 solver = NonlinearVariationalSolver(problem)
 solver.parameters['newton_solver']['relative_tolerance'] = 1e-6
 solver.parameters['newton_solver']['linear_solver'] = 'mumps'
+
 
 # Time stepping parameters
 dt = 0.1
@@ -116,9 +203,10 @@ stress_vec = []
 stress_ana = []
 
 while t <= T:
+    print('time: ', t)
 
-        # Increase traction
-
+    # Increase traction
+    h.t = t
     tDirBC.time_ = t
 
     # solve and save disp
@@ -136,7 +224,8 @@ while t <= T:
     stretch_vec.append(defGrad(point)[0])
 
     # get stress at a point for plotting
-    PK1_s, thydpress = pk1Stress(u_plot, p_plot, E, nu)
+    PK1_s, thydpress = pk1Stress(
+        u_plot, p_plot, E, nu, mu1, mu2, mu3, mu4, beta3, beta4, a1, a2)
     PK1_stress.assign(project(PK1_s, W_DFnStress))
     stress_vec.append(PK1_stress(point)[0])
 
@@ -148,15 +237,15 @@ while t <= T:
     # time increment
     t += float(dt)
 
+
 # get analytical solution
 stretch_vec = np.array(stretch_vec)
 stress_vec = np.array(stress_vec)
 G = E / (2 * (1 + nu))
 c1 = G / 2.0
 for i in range(len(stretch_vec)):
-   # PK1
-    pk1_ana = 2 * c1 * (stretch_vec[i] -
-                        1 / (stretch_vec[i] * stretch_vec[i]))
+    pk1_ana = 2 * c1 * (stretch_vec[i] - 1 /
+                        (stretch_vec[i] * stretch_vec[i]))  # PK1
     pk2_ana = (1 / stretch_vec[i]) * pk1_ana  # PK2
     stress_ana.append(pk2_ana)
 stress_ana = np.array(stress_ana)
@@ -164,4 +253,9 @@ stress_ana = np.array(stress_ana)
 # plot results
 f = plt.figure(figsize=(12, 6))
 plt.plot(stretch_vec, stress_vec, 'r-')
+plt.savefig('test1.png')
 plt.plot(stretch_vec, stress_ana, 'k.')
+plt.savefig('test2.png')
+
+file = File(output_dir + "displacements.pvd")
+file << u_plot
